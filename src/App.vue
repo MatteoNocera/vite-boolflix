dd79ea7335640697f9d5f3de0ca864867ac4c620
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-28 16:40:10 +0200

    insert flags

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script>
 import { state } from './state.js'
-import { hasFlag } from 'country-flag-icons'
 export default {
   name: 'App',
   data() {
     return {
       state,
+      url_flags: 'https://flagsapi.com/',
 
     }
   },
@@ -15,6 +15,7 @@ export default {
       const url = this.state.base_url + `&query=${state.filterName}`;
       this.state.searchFilm(url)
     },
+
 
   }
 }
@@ -26,12 +27,24 @@ export default {
       <input type="search" name="search_film" id="search_film" v-model="state.filterName">
       <button @click="this.filterByName()">Cerca</button>
     </div>
-    <img src=http://purecatamphetamine.github.io/country-flag-icons/3x2/IT.svg height="20" alt="">
+    <img height="20" alt="">
     <ol>
       <li v-for="film in state.filmData">
         <h3>Title: {{ film.title }}</h3>
         <h5>Original Title: {{ film.original_title }}</h5>
-        <p>Language: {{ film.original_language }}</p>
+        <p>
+          Language: {{ film.original_language.toUpperCase() }}
+
+          <img v-if="film.original_language == 'en'" :src="url_flags + 'GB/shiny/24.png'" alt="">
+
+          <img v-else-if="film.original_language == 'ja'" :src="url_flags + 'JP/shiny/24.png'" alt="">
+
+          <img v-else-if="film.original_language == 'zh'" :src="url_flags + 'CN/shiny/24.png'" alt="">
+
+          <img v-else :src="url_flags + film.original_language.toUpperCase() + '/shiny/24.png'" alt="">
+        </p>
+
+
 
         <p>Vote: {{ film.vote_average }}</p>
 
--- src/state.js
+++ src/state.js
@@ -10,6 +10,7 @@ export const state = reactive({
 
 
 
+
     // Actions
     searchFilm(url) {
         axios
