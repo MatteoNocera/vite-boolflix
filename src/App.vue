9ea2072ebd62dbed4fd94e3cc1d976e44221cd98
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-28 18:52:58 +0200

    Add LanguageItem content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <script>
 import { state } from './state.js'
+import LanguageItem from './components/LanguageItem.vue';
 export default {
   name: 'App',
   data() {
     return {
       state,
       url_flags: 'https://flagsapi.com/',
-
-
-    }
+    };
   },
   methods: {
     filterByFilm() {
       console.log('searching...film');
       const url = this.state.film_base_url + `&query=${state.filterName}`;
-      this.state.searchFilm(url)
+      this.state.searchFilm(url);
     },
     filterBySeries() {
       console.log('searching...series');
       const url_series = this.state.series_base_url + `&query=${state.filterName}`;
-      this.state.searchSeries(url_series)
-
+      this.state.searchSeries(url_series);
     }
-
+  },
+  components: {
+    LanguageItem,
   }
 }
 </script>
@@ -31,7 +31,7 @@ export default {
   <div class="container my-3">
     <div class="searchbar">
       <input type="search" name="search_film" id="search_film" v-model="state.filterName">
-      <button @click="this.filterBySeries(), this.filterByFilm()">Cerca</button>
+      <button @click="this.filterByFilm(), this.filterBySeries()">Cerca</button>
     </div>
 
     <div class="row">
@@ -39,21 +39,14 @@ export default {
         <h2>Film</h2>
         <ol>
           <li v-for="film in state.filmData">
-            <h3>Title: {{ film.title }}</h3>
-            <h5>Original Title: {{ film.original_title }}</h5>
-            <p>
-              Language: {{ film.original_language.toUpperCase() }}
+            <h3>Titolo: {{ film.title }}</h3>
+            <h5>Titolo Originale: {{ film.original_title }}</h5>
 
-              <img v-if="film.original_language == 'en'" :src="url_flags + 'GB/shiny/24.png'" alt="">
+            <LanguageItem :position="film.original_language.toUpperCase()" :url="url_flags"></LanguageItem>
 
-              <img v-else-if="film.original_language == 'ja'" :src="url_flags + 'JP/shiny/24.png'" alt="">
+            <p>Voto: {{ film.vote_average.toFixed(0) }}</p>
 
-              <img v-else-if="film.original_language == 'zh'" :src="url_flags + 'CN/shiny/24.png'" alt="">
 
-              <img v-else :src="url_flags + film.original_language.toUpperCase() + '/shiny/24.png'" alt="">
-            </p>
-
-            <p>Vote: {{ film.vote_average.toFixed(0) }}</p>
 
           </li>
         </ol>
@@ -63,21 +56,12 @@ export default {
         <h2>Serie TV</h2>
         <ol>
           <li v-for="serie in state.seriesData">
-            <h3>Title: {{ serie.name }}</h3>
-            <h5>Original Title: {{ serie.original_name }}</h5>
-            <p>
-              Language: {{ serie.original_language.toUpperCase() }}
+            <h3>Titolo: {{ serie.name }}</h3>
+            <h5>Titolo Originale: {{ serie.original_name }}</h5>
 
-              <img v-if="serie.original_language == 'en'" :src="url_flags + 'GB/shiny/24.png'" alt="">
+            <LanguageItem :position="serie.original_language.toUpperCase()" :url="url_flags"></LanguageItem>
 
-              <img v-else-if="serie.original_language == 'ja'" :src="url_flags + 'JP/shiny/24.png'" alt="">
-
-              <img v-else-if="serie.original_language == 'zh'" :src="url_flags + 'CN/shiny/24.png'" alt="">
-
-              <img v-else :src="url_flags + serie.original_language.toUpperCase() + '/shiny/24.png'" alt="">
-            </p>
-
-            <p>Vote: {{ serie.vote_average.toFixed(0) }}</p>
+            <p>Voto: {{ serie.vote_average.toFixed(0) }}</p>
 
           </li>
         </ol>
---
+++ src/components/LanguageItem.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'LanguageItem',
+    props: ['position', 'url']
+}
+</script>
+
+<template>
+    <p>
+        Lingua: {{ position }}
+
+        <img v-if="position == 'EN'" :src="url + 'GB/shiny/24.png'" alt="">
+
+        <img v-else-if="position == 'JA'" :src="url + 'JP/shiny/24.png'" alt="">
+
+        <img v-else-if="position == 'ZH'" :src="url + 'CN/shiny/24.png'" alt="">
+
+        <img v-else :src="url + position + '/shiny/24.png'" alt="">
+    </p>
+</template>
+
+
+
+<style></style>
